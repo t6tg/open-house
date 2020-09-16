680634e3121d0ab1aa44304e472428945b821507
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-16 20:47:34 +0700

    update getOS

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import liff from "@line/liff";
 
+const getEnv = () => {
+  document.getElementById("os").append(liff.getOS());
+};
+
 const main = async () => {
   liff.ready.then(() => {
-    document.write(liff.getOS());
+    getEnv();
   });
   await liff.init({ liffId: "1654164368-LmQGP5pp" });
 };
