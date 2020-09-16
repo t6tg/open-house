a8636a5c0b68466d2b603006c3d3f03af8361048
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-16 20:54:06 +0700

    set OS

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const getEnv = () => {
   document.getElementById("os").append(liff.getOS());
 };
 
-const main = async () => {
+async function main() {
   liff.ready.then(() => {
     getEnv();
   });
   await liff.init({ liffId: "1654164368-LmQGP5pp" });
-};
-
+}
 main();
