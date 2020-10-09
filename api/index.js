b64505084d86191185736aafd58b2c69020c1638
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-09 16:22:37 +0700

    delete route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,26 @@
 const fastify = require("fastify");
-const router = require("./router.js");
+const line = require("@line/bot-sdk");
+require("dotenv").config();
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
 function build() {
   const app = fastify({
     logger: true,
   });
-
+  const client = new line.Client(config);
+  app.post("api/v1/register", async (req, res) => {
+    const { userId } = req.body;
+    client.linkRichMenuToUser(
+      userId,
+      "richmenu-53012715ee822676ac82c99611d2245c"
+    );
+  });
   app.register(require("fastify-cors"));
   app.register(require("fastify-formbody"));
-  app.register(router, { prefix: "/api/v1" });
 
   return app;
 }
--- api/router.js
+++
@@ -1,4 +0,0 @@
-import indexRoute from "./controllers/index";
-export default async function router(fastify) {
-  fastify.register(indexRoute, { prefix: "/" });
-}
