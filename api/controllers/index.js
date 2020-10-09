6b72c73707706ba6b807fc2dc2fcff3edfbf85bd
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-09 16:13:32 +0700

    comment

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,18 +1,18 @@
-const line = require("@line/bot-sdk");
-require("dotenv").config();
+// const line = require("@line/bot-sdk");
+// require("dotenv").config();
 
-const client = new line.Client(config);
+// const client = new line.Client(config);
 
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
+// const config = {
+//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+//   channelSecret: process.env.CHANNEL_SECRET,
+// };
 export default async function index(fastify) {
   fastify.post("/register", async (req, res) => {
     const { userId } = req.body;
-    client.linkRichMenuToUser(
-      userId,
-      "richmenu-53012715ee822676ac82c99611d2245c"
-    );
+    // client.linkRichMenuToUser(
+    //   userId,
+    //   "richmenu-53012715ee822676ac82c99611d2245c"
+    // );
   });
 }
