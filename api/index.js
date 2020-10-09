639169746a4e7a7d8c098a5b9dfd99284ef62038
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-09 13:52:11 +0700

    line sdk test

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 const fastify = require("fastify");
 const router = require("./router.js");
+const line = require("@line/bot-sdk");
+require("dotenv").config();
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
 function build() {
   const app = fastify({
     logger: true,
   });
+  const client = new line.Client(config);
+  app.register(require("fastify-cors"));
+  app.register(require("fastify-formbody"));
   app.register(router);
+
   return app;
 }
 
