38a5e2f2cac964b847da4228f77d5e6a1a41b0de
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-09 11:09:16 +0700

    api config serverless

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
+const btn = document.getElementById("btn");
+let userId;
+
 const getPro = async () => {
   const p = await liff.getProfile();
   document.getElementById("pictureUrl").src = p.pictureUrl;
   document.getElementById("liff_name").innerText = p.displayName;
   document.getElementById("email").value = liff.getDecodedIDToken().email;
+  userId = p.userId;
 };
 
-async function main() {
+function main() {
+  liff.init({ liffId: "1654164368-LmQGP5pp" });
   liff.ready.then(() => {
-    if (liff.isInClient()) {
+    if (liff.isLoggedIn()) {
       getPro();
     } else {
-      document.write("กรุณาเข้าผ่าน Line บนมือถือเท่านั้น");
+      liff.login();
     }
   });
-  await liff.init({ liffId: "1654164368-LmQGP5pp" });
 }
+
 main();
+
+async function send(e) {
+  e.preventDefault();
+  const name = document.getElementById("regis_name").value;
+  const email = document.getElementById("email").value;
+  const regis_class = document.getElementById("regis_class").value;
+  const school = document.getElementById("school").value;
+
+  try {
+    const result = await axios.post("localhost:3000/api/v1/register", {
+      name,
+      email,
+      regis_class,
+      school,
+      userId,
+    });
+    if (result.status == 200) {
+      console.log("done");
+      liff.closeWindow();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
